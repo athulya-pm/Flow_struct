9f7e11aa4bd33f5037fa77d55caf8127d62ba6c2
athulya-pm <97933831+athulya-pm@users.noreply.github.com>
2023-09-28 14:49:23 +0530

    Update Flow_struct.sol

--- Flow_struct.sol
+++ Flow_struct.sol
@@ -1,28 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.;
+pragma solidity 0.8.18;
 
 contract MyContract {
     // Define a struct
     struct Person {
         string name;
-        uint256 age;
+        uint age;
     }
 
     // Declare an array of the struct type
     Person[] public people;
 
     // Function to add a new person to the array
-    function addPerson(string memory _name, uint256 _age) public {
+    function addPerson(string memory _name, uint _age) public {
         people.push(Person(_name, _age));
     }
 
     // Function to get the number of people in the array
-    function getPeopleCount() public view returns (uint256) {
+    function getPeopleCount() public view returns (uint) {
         return people.length;
     }
 
     // Function to get the details of a person at a specific index in the array
-    function getPerson(uint256 index) public view returns (string memory, uint256) {
+    function getPerson(uint index) public view returns (string memory, uint) {
         require(index < people.length, "Index out of bounds");
         return (people[index].name, people[index].age);
     }
